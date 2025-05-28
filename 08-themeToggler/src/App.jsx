ffa1f632a681e7e948a38adae64d2b77c53a4648
Nadim Khan <nadimkhann47@gmail.com>
2025-05-28 12:53:35 +0530

    theme toggler

--- 08-themeToggler/src/App.jsx
+++ 08-themeToggler/src/App.jsx
@@ -4,21 +4,21 @@ import Card from "./components/Card";
 import ThemeBtn from "./components/ThemeBtn";
 
 function App() {
-  const [themMode, setThemMode] = useState("light");
+  const [themeMode, setThemeMode] = useState("light");
 
   const lightTheme = () => {
-    setThemMode("light");
+    setThemeMode("light");
   };
   const darkTheme = () => {
-    setThemMode("dark");
+    setThemeMode("dark");
   };
 
   useEffect(() => {
     document.querySelector("html").classList.remove("light", "dark");
-    document.querySelector("html").classList.add(themMode);
-  }, [themMode]);
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
   return (
-    <ThemeProvider value={{ themMode, darkTheme, lightTheme }}>
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
